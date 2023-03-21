036034e7a143f4295bfa9c053498e63e4bc1ee23
Yasin Erdemli <yasinerdemli35@icloud.com>
2023-03-21 19:13:38 +0300

    Initial Commit

---
+++ NewSwiftUILayout/NewSwiftUILayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewSwiftUILayoutApp.swift
+//  NewSwiftUILayout
+//
+//  Created by Yasin Erdemli on 21.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewSwiftUILayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
